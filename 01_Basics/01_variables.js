31696b226824172029972c26239f1e5b450834b9
Ayush-0021 <ayush009521@gmail.com>
2025-08-13 21:46:29 +0530

    basics

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1234567890; 
+let accountEmail = "abc@gmail.com"
+var accountPassword = "9999999999"
+accountCity = "Patna"
+let accountState;
+
+// accountId = 2 not allowed, as const cannot be reassigned//
+accountEmail ="xyz@gmail.com"
+accountPassword = "0000000000"
+accountCity = "Ranchi"
+
+
+console.log(accountId); 
+console.log([accountId,accountEmail, accountPassword, accountCity, accountState]); 
+// Preffer not to use var because of issue in block scope and functional scope//
+
+
+
