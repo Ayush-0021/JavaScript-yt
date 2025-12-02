1ffba881720583fb6f126f7abfac3b0cf80ca4bd
Ayush-0021 <ayush009521@gmail.com>
2025-12-02 20:04:24 +0530

    conversionoperation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,4 +1,30 @@
-let score = 45;
+let score = "Hitesh";
 
 console.log(typeof score);
 console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+// "45"=>45
+// "45abc"=>NaN
+// true=>1; false=>0
+// null=>0
+// undefined=>NaN
+
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+// 1=>true; 0=>false
+// ""=>false; "Nishant"=>true
+// null=>false
+// undefined=>false
+// NaN=>false
+
+
+let someNumber = 34;
+let stringSomeNumber = String(someNumber);
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
+// 34=> "34"
