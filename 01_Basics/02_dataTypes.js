db35f51e474883763ce0c5a1260f65512673baac
Ayush-0021 <ayush009521@gmail.com>
2025-12-02 19:41:23 +0530

    dataType

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -1,8 +1,32 @@
 "use strict"; // treat all JS code as newer version //
 
-// alert( 3+3) // we are using node.js, not browser
+// alert( 3+3 ) // we are using node.js, not browser
 
 console.log(3
-    +3)
+    +3) 
     //code readability should be high//
 console.log("Hello world!");
+
+// Data Types in JS//
+let name = "Nishant";
+let age = 21;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
+
+// number => 2 to the power of 53
+// bigint => larger than 2 to the power of 53
+// string=> '',"",``
+// boolean => true/false
+// undefined => value not assigned
+// null => empty value
+// symbol => unique identifiers
+
+
+// object => key value pairs
+
+console.log(typeof "Nishant");
+console.log(typeof 21);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
